e8ade0dc3fe4b2fb7c98a03a1430da807e4d070f
kcly3027 <lc_liuyang@163.com>
2018-04-08 17:29:40 +0800

    start

--- btcpool_start.sh
+++ btcpool_start.sh
@@ -1,31 +1,31 @@
 #启动gbtmaker
 cd /work/btcpool/build/run_gbtmaker
-./gbtmaker -c ./gbtmaker.cfg -l ./log_gbtmaker &
+nohup ./gbtmaker -c ./gbtmaker.cfg -l ./log_gbtmaker  > /dev/null &
 
 #启动jobmaker
 cd /work/btcpool/build/run_jobmaker
-./jobmaker -c ./jobmaker.cfg -l ./log_jobmaker &
+nohup ./jobmaker -c ./jobmaker.cfg -l ./log_jobmaker  > /dev/null &
 
 #启动sserver
 cd /work/btcpool/build/run_sserver
-./sserver -c ./sserver.cfg -l ./log_sserver &
+nohup ./sserver -c ./sserver.cfg -l ./log_sserver  > /dev/null &
 
 #启动blkmaker
 cd /work/btcpool/build/run_blkmaker
-./blkmaker -c ./blkmaker.cfg -l ./log_blkmaker &
+nohup ./blkmaker -c ./blkmaker.cfg -l ./log_blkmaker  > /dev/null &
 
 #启动sharelogger
 cd /work/btcpool/build/run_sharelogger
-./sharelogger -c ./sharelogger.cfg -l ./log_sharelogger &
+nohup ./sharelogger -c ./sharelogger.cfg -l ./log_sharelogger  > /dev/null &
 
 #启动slparser
 cd /work/btcpool/build/run_slparser
-./slparser -c ./slparser.cfg -l ./log_slparser &
+nohup ./slparser -c ./slparser.cfg -l ./log_slparser  > /dev/null &
 
 #启动sharelogger
 cd /work/btcpool/build/run_statshttpd
-./statshttpd -c ./statshttpd.cfg -l ./log_statshttpd &
+nohup ./statshttpd -c ./statshttpd.cfg -l ./log_statshttpd  > /dev/null &
 
 #启动poolwatcher 
 cd /work/btcpool/build/run_poolwatcher 
-./poolwatcher -c ./poolwatcher.cfg -l ./log_poolwatcher &
+nohup ./poolwatcher -c ./poolwatcher.cfg -l ./log_poolwatcher  > /dev/null &
